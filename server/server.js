bf3f4c6cb05993637036fa660887f310d159ab91
jxhnkndl <jkroyston@gmail.com>
2023-01-24 18:39:21 -0500

    configure apollo server and install apollo server and graphql dependencies

--- server/server.js
+++ server/server.js
@@ -1,14 +1,38 @@
 const express = require('express');
+
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas/index');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+
+// create apollo server using schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+// create instance of apollo server using graphql schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+
+  // add express app to apollo server as middleware
+  // this creates single /graphql api endpoint
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`GraphQL Playground @ http://localhost:${PORT}${server.graphqlPath}`);
+    });
   });
-});
+};
+
+// start apollo server
+startApolloServer(typeDefs, resolvers);
