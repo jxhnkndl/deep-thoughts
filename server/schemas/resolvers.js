072ff84986a2ae03e0e7bd3833c22b64a3244a47
jxhnkndl <jkroyston@gmail.com>
2023-01-24 21:08:54 -0500

    create mutations for adding thoughts, reactions, and friends

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,26 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User, Thought, Reaction } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    // get logged in user
+    me: async (parent, args, context) => {
+      // verify user has passed auth check
+      if (context.user) {
+        const { _id } = context.user.data;
+
+        const userData = await User.findOne({ _id })
+          .select('-__v -password')
+          .populate('friends')
+          .populate('thoughts');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('User must be logged in');
+    },
+
     // get all users
     users: async () => {
       return User.find()
@@ -63,6 +80,65 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
+    },
+
+    // add new thought
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const { username, _id } = context.user.data;
+
+        // create thought
+        const thought = await Thought.create({
+          ...args,
+          username,
+        });
+
+        // push thought into author's user data
+        await User.findByIdAndUpdate(
+          { _id },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+
+        return thought;
+      }
+
+      throw new AuthenticationError('User must be logged in');
+    },
+
+    // add reaction to thought
+    addReaction: async (parent, { reactionBody, thoughtId }, context) => {
+      if (context.user) {
+        const { username } = context.user.data;
+
+        const updatedThought = await Thought.findByIdAndUpdate(
+          { _id: thoughtId },
+          { $push: { reactions: { reactionBody, username } } },
+          { new: true, runValidators: true }
+        );
+
+        return updatedThought;
+      }
+
+      throw new AuthenticationError('User must be logged in');
+    },
+
+    // add friend to user profile
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const { _id } = context.user.data;
+
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id },
+          // $addToSet because user can only be friends with another user once
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate('friends');
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('User must be logged in');
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,6 +33,7 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
@@ -42,6 +43,9 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: String!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
